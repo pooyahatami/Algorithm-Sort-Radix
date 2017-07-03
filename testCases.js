ab5ae08349c57bbdd209673ea762a919c047eba9
Pooya Hatami <am.hatami@yahoo.com>
2017-07-02 22:00:10 -0700

    Update testCases.js

--- testCases.js
+++ testCases.js
@@ -15,8 +15,12 @@
  * Author: Pooya Hatami
  */
 
-var rectcs = require('./node-sort-radix');
 
+var nodesort = require('./node-sort-radix');
+var displaymode = "No"; //"Yes";  // "Yes" for more details of algorithm progress 
+var base = 10;  //  (Decimal Base 10 , Hex Base 16 , Octal Base 8 , Binary Base 2 ).
+
+var arrin00 = [20, 8 , -11, 12, 22 , 9 , 10 ];
 var arrin01 = [20, 8 , 48, 120, 220 , 390 , 1000 ];
 var arrin02 = [20, 8 , 480 , 120, 220 , 390 , 1000 ];
 var arrin03 = [1120, 800 , 480 , 120, 20 , 390 , 1000 ];
@@ -28,27 +32,42 @@ var arrin06 = [1, 3, -7, 25, 12, 9, 8,
                       121, 221, -10, 18, 29, 49];
 var arrin07 = [1, 3, 7000000000000000000, 25, 12, 9, 8,
                       121, 221, 100000000000000000000000000 , 18, 290000000000000000000, 49];
-var arrin07 = [1, 3, 75432, 25, 12, 9, 8,
-                      121, 221, 976562 , 18, 2900000, 49];
-//var arrin08 = [A7,02,22,77,37,15,00,40,B00,75,04,05,07,75,52,12,50,77,71,D07];
-var arrin09 = [1001,101010,11,10,01,111,100,1000,11100,10110,101,100010,0111,101,11111,1000001,1,0,111,11010];
-var arrin10 = [7,2,22,77,37,15,10770,740,70,75,04,5,107,75,52,12,50,177,71,207];
+var arrin08 = [1, 3, 75432, 25, 12, 9, 8,
+                      121, 221, 976562 , 18, 299999, 49];
+var arrin09 = [0.897, 0.565, 0.656, 0.1234, 0.665, 0.3434 , 0.611 , 0.621 ];
+var arrin10 = [1,342, 14,293 , 0.897, 0.565, 0.656, 0.1234, 0.665, 0.3434 , 0.611 , 0.621 ];
+var arrin11 = [5, 8 , 11, 12, 2 , 9 , 10 , 4 , 11, 10, 12, 7, 9 ];
+var arrin12 = "";
+//var arrin13 = [A7,02,22,77,37,15,00,40,B00,75,04,05,07,75,52,12,50,77,71,D07];    //base16
+var arrin14 = [1001,101010,11,10,01,111,100,1000,11100,10110,101,100010,0111,101,11111,1000001,1,0,111,11010];   //base 2
+var arrin15 = [7,2,22,77,37,15,10770,740,70,75,04,5,107,75,52,12,50,177,71,207];   //base 8
 
-function solveRS(arr,base) {
-    var arr_original = arr.toString() ;
-    var result = rectcs.radixSort(arr,base);
-    if (result==-1){
-    console.log("Fail attempt to sort array \r\n  ["+arr_original+" ] by Insertion Sort " );
-    } else {
-    console.log("Success attempt to sort array \r\n \t ["+arr_original+" ] \r\n and result is : \r\n \t [ "
+
+function solveSorting(inputArray,base) {
+    var arr_original = inputArray.toString() ;
+    var sortedArray = inputArray;
+
+    nodesort(inputArray, displaymode,  function(err,sortRef) {
+        if (err) {
+	         console.log(err);
+	                }
+	      else {
+           var result = sortRef.radixSort(inputArray,base);
+	         console.log("Success attempt to sort array \r\n \t ["+arr_original+" ] \r\n and result is : \r\n \t [ "
                 + result + " ]" );
-    }
-   
-   console.log("----------------------------------------------------------");     
-}
+  
+	      sortedArray = result;
+	            }
+	      console.log("----------------------------------------------------------"); 
+    });
+    
+    return sortedArray;
+};
 
-solveRS(arrin01,10);
-solveRS(arrin05,10);
-//solveRS(arrin08,16);
-solveRS(arrin09,2);
-solveRS(arrin10,8);
+solveSorting(arrin01,10);
+solveSorting(arrin02,10);
+solveSorting(arrin06,10);
+solveSorting(arrin11,10);
+solveSorting(arrin12,10);
+solveSorting(arrin14,2);
+solveSorting(arrin15,8);
